af642747af58cb0282daa4509373d01df426f58c
ananda <anandafit@gmila.com>
2018-11-20 12:20:13 +0530

    sample code for multiple row insert

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,9 +1,5 @@
-/**
- * Created by ananda on 11/4/18.
- */
-import _ from 'lodash';
 import { ServerError } from 'Errors';
-import User from '../repository/User';
+import UserRepo from '../repository/User';
 import {UserManager}  from 'Managers'
 
 
@@ -11,7 +7,8 @@ const updateUserByUserId = async (req) => {
   try {
     const id = req.body.id;
     const name = req.body.name;
-    return User.createUser( id, name);
+    const users = [{'ID':id, 'NAME':name}];
+    return UserRepo.insertUsers(users);
   }catch (e){
     throw new ServerError("server error");
   }
@@ -19,7 +16,7 @@ const updateUserByUserId = async (req) => {
 
 const getUserByUserId = async (req) => {
   const uid = req.params.uid;
-  return User.fetchUserById( uid );
+  return UserRepo.fetchUserById( uid );
 };
 
 export default {
