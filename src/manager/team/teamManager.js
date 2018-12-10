ccb3687f7e897c1a05e9d2e7e5017fd8d064e90f
mrclemrkz <awesomeisme007@gmail.com>
2018-12-10 21:19:34 +0530

    update on get: /teams/{team_id}

--- src/manager/team/teamManager.js
+++ src/manager/team/teamManager.js
@@ -5,7 +5,15 @@ const resultMaps = [
   {
     mapId: 'teamMap',
     idProperty: 'id',
-    properties: ['id', 'name']
+    properties: ['id', 'name', 'symbol', 'fax', 'name_of_authorized_member', 'address_of_authorized_member'],
+    collections: [
+      { name: 'contacts', mapId: 'contactMap', columnPrefix: 'contact_' }
+    ]
+  },
+  {
+    mapId: 'contactMap',
+    idProperty: 'id',
+    properties: ['no']
   }
 ];
 
@@ -13,10 +21,11 @@ const resultMaps = [
 const mapToTeamModel = (teams) => {
   const mappedTeams = joinjs.map(teams, resultMaps, 'teamMap', 'team_');
 
-  return Team({
-    id: mappedTeams[0].id,
-    name: mappedTeams[0].name,
-  });
+  // return Team({
+  //   id: mappedTeams[0].id,
+  //   name: mappedTeams[0].name,
+  // });
+  return mappedTeams;
 };
 
 export default {
