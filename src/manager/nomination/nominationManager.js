e336268e7e46f79f6d1a4ed3fef3fc7faeb692e9
mrclemrkz <awesomeisme007@gmail.com>
2018-12-17 10:06:59 +0530

    update on: get: /elections/{election_id}/teams/{team_id}/nominations/:status
    witout model mapping

--- src/manager/nomination/nominationManager.js
+++ src/manager/nomination/nominationManager.js
@@ -1,5 +1,6 @@
 import { Nomination } from 'Models';
 var joinjs = require('join-js').default;
+const { Map, List } = require('immutable');
 
 const resultMaps = [
     {
@@ -12,13 +13,16 @@ const resultMaps = [
 const mapToNominationModel = (nominationData) => {
     const mappedNominations = joinjs.map(nominationData, resultMaps, 'nominationMap', 'nomination_');
 
-    return Nomination({
-        id: mappedNominations[0].id,
-        status: mappedNominations[0].status,
-        teamId: mappedNominations[0].team_id,
-        electionId: mappedNominations[0].election_id,
-        divisionConfigDataId: mappedNominations[0].division_config_data_id,
-    });
+    // return Nomination({
+    //     id: mappedNominations[0].id,
+    //     status: mappedNominations[0].status,
+    //     teamId: mappedNominations[0].team_id,
+    //     electionId: mappedNominations[0].election_id,
+    //     divisionConfigDataId: mappedNominations[0].division_config_data_id,
+    // });
+    
+    return mappedNominations;
+
 };
 
 export default {
