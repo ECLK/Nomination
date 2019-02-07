a9b155cbba636663ffb975d4daacf500b35d0455
mrclemrkz <awesomeisme007@gmail.com>
2019-02-07 14:40:03 +0530

    put /candidate/:candidate_id

--- src/repository/candidate.js
+++ src/repository/candidate.js
@@ -31,48 +31,92 @@ const CANDIDATE_BY_CANDIDATE_ID_SELECT_QUERY = `SELECT ID AS CANDIDATE_ID,
                                              FROM CANDIDATE WHERE NOMINATION_ID = :nominationId AND ID = :candidateId`;
 
 const CANDIDATE_INSERT_QUERY = `INSERT INTO CANDIDATE (ID, FULL_NAME, PREFERRED_NAME, NIC, DATE_OF_BIRTH, GENDER, ADDRESS, OCCUPATION, ELECTORAL_DIVISION_NAME, ELECTORAL_DIVISION_CODE, COUNSIL_NAME, NOMINATION_ID) 
-                              VALUES (:id, :fullName,:preferredName, :nic, :dateOfBirth, :gender, :address,:occupation, :electoralDivisionName, :electoralDivisionCode, :counsilName , :nominationId)`;
+							  VALUES (:id, :fullName,:preferredName, :nic, :dateOfBirth, :gender, :address,:occupation, :electoralDivisionName, :electoralDivisionCode, :counsilName , :nominationId)`;
+							  
 
 
 const getCandidateListByNomination = (nomination_id) => {
-  const params = { nomination_id: nomination_id };
-  return DbConnection()
-    .query(CANDIDATE_BY_NOMINATION_SELECT_QUERY,
-      {
-        replacements: params,
-        type: DbConnection().QueryTypes.SELECT,
-      }).catch((error) => {
-        throw new DBError(error);
-      });
+	const params = { nomination_id: nomination_id };
+	return DbConnection()
+		.query(CANDIDATE_BY_NOMINATION_SELECT_QUERY,
+			{
+				replacements: params,
+				type: DbConnection().QueryTypes.SELECT,
+			}).catch((error) => {
+				throw new DBError(error);
+			});
 }
 
 const getCandidateByNomination = (params) => {
-  return DbConnection()
-    .query(CANDIDATE_BY_CANDIDATE_ID_SELECT_QUERY,
-      {
-        replacements: params,
-        type: DbConnection().QueryTypes.SELECT,
-      }).catch((error) => {
-        throw new DBError(error);
-      });
+	return DbConnection()
+		.query(CANDIDATE_BY_CANDIDATE_ID_SELECT_QUERY,
+			{
+				replacements: params,
+				type: DbConnection().QueryTypes.SELECT,
+			}).catch((error) => {
+				throw new DBError(error);
+			});
 }
 
 const createCandidate = (candidateData) => {
-  const params = candidateData;
-  return DbConnection()
-    .query(CANDIDATE_INSERT_QUERY,
-      {
-        replacements: params,
-        type: DbConnection().QueryTypes.INSERT,
-      }).then((results) => {
-       return params ;
-      }).catch((error) => {
-        throw new DBError(error);
-      });
+	const params = candidateData;
+	return DbConnection()
+		.query(CANDIDATE_INSERT_QUERY,
+			{
+				replacements: params,
+				type: DbConnection().QueryTypes.INSERT,
+			}).then((results) => {
+				return params;
+			}).catch((error) => {
+				throw new DBError(error);
+			});
 };
 
+
+const CANDIDATE_BY_ID_UPDATE_QUERY = `UPDATE CANDIDATE
+SET 
+	FULL_NAME = :fullName,
+	PREFERRED_NAME = :preferredName,
+	NIC = :nic,
+	DATE_OF_BIRTH = :dateOfBirth,
+	GENDER = :gender,
+	ADDRESS = :address,
+	OCCUPATION = :occupation, 
+	ELECTORAL_DIVISION_NAME = :electoralDivisionName,
+	ELECTORAL_DIVISION_CODE = :electoralDivisionCode,
+	COUNSIL_NAME = :counsilName
+WHERE ID = :id`;
+const updateCandidate = (candidateData) => {
+	const params = candidateData;
+	return DbConnection()
+		.query(CANDIDATE_BY_ID_UPDATE_QUERY,
+			{
+				replacements: params,
+				type: DbConnection().QueryTypes.UPDATE
+			}).then(() => {
+				return candidateData;
+			}).catch((error) => {
+				throw new DBError(error);
+			});
+}
+
+const CANDIDATE_BY_ID_SELECT_QUERY = `SELECT * FROM CANDIDATE WHERE ID = :id`;
+const getCandidateById = (candidateId) => {
+	const params = { id: candidateId };
+	return DbConnection()
+		.query(CANDIDATE_BY_ID_SELECT_QUERY,
+			{
+				replacements: params,
+				type: DbConnection().QueryTypes.SELECT
+			}).catch((error) => {
+				throw new DBError(error);
+			})
+}
+
 export default {
-  getCandidateListByNomination,
-  createCandidate,
-  getCandidateByNomination,
+	getCandidateListByNomination,
+	createCandidate,
+	getCandidateByNomination,
+	getCandidateById,
+	updateCandidate
 }
