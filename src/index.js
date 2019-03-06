aadec20c510b95f1ca2117e3d9c16760ec6b8df4
ananda <anandafit@gmila.com>
2019-03-06 14:04:32 +0530

    adding config injecting from out side

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import './middleware/express';
+import { injectAppConfigs } from './config/ConfigService';
 
+injectAppConfigs();
 process.on('uncaughtException', error => {
   throw new Error(error, 'Uncaught exception');
 });
