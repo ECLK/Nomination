dd9b203cac278e07be4e15ad6f2902073b3420aa
manuranga perera <manu@wso2.com>
2019-03-20 17:19:41 +0530

    Move port config to json to make dockerization easy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import 'dotenv/config';
-import './middleware/express';
 import { injectAppConfigs } from './config/ConfigService';
 
 injectAppConfigs();
+
+require('./middleware/express');
+
 process.on('uncaughtException', error => {
   throw new Error(error, 'Uncaught exception');
 });
